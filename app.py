044982567773db3c595b431736db99a25edf5a0e
Markus Nießen <markusniessen1@gmail.com>
2026-07-04 18:54:36 +0200

    Integrate DataManager with users route

--- app.py
+++ app.py
@@ -1,24 +1,30 @@
 from flask import Flask
+
+from models import db
 from data_manager import DataManager
-from models import db, Movie
 
 app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///moviweb.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///movies.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db.init_app(app)
 
 data_manager = DataManager()
 
+with app.app_context():
+    db.create_all()
+
 
 @app.route("/")
 def home():
-    return "Welcome to MoviWeb App!"
+    return "Welcome to MoviWeb!"
 
 
-with app.app_context():
-    db.create_all()
+@app.route("/users")
+def list_users():
+    users = data_manager.get_users()
+    return str(users)
 
 
 if __name__ == "__main__":
